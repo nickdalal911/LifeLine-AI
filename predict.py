6cf51ceb41d0234ee9de71b4316c8fb0fcaee6ec
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2026-03-19 23:35:03 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,17 +1,40 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+import os
+
+try:
+    import tensorflow as tf
+    from tensorflow.keras.models import load_model
+    TF_AVAILABLE = True
+except:
+    TF_AVAILABLE = False
 from tensorflow.keras.applications.efficientnet import preprocess_input
 import cv2
+
+model = None
+
+if TF_AVAILABLE and os.path.exists("best_model.h5"):
+    model = load_model("best_model.h5")
+
+
 
 model = load_model("best_model.h5")
 classes = ["First Degree", "Second Degree", "Third Degree"]
 
 def predict_burn(image):
-    img = cv2.resize(image, (224, 224))
-    img = preprocess_input(img)
-    img = np.expand_dims(img, axis=0)
-    pred = model.predict(img)
-    confidence = float(np.max(pred) * 100)
-    label = classes[np.argmax(pred)]
-    return label, confidence
+    # If model not available
+    if model is None:
+        return "⚠️ Model not available", 0.0
+
+    try:
+        img = cv2.resize(image, (224, 224))
+        img = preprocess_input(img)
+        img = np.expand_dims(img, axis=0)
+
+        pred = model.predict(img)
+        confidence = float(np.max(pred) * 100)
+        label = classes[np.argmax(pred)]
+
+        return label, confidence
+
+    except Exception as e:
+        return f"Error: {str(e)}", 0.0
